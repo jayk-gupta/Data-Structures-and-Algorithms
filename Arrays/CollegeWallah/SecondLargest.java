f2d9dc1f08967a4e1c9fbd6e61b78f3893009780
Jay Kumar Gupta <100681165+jayk-gupta@users.noreply.github.com>
2023-04-25 16:45:20 +0530

    updated second largest ques

--- Arrays/CollegeWallah/SecondLargest.java
+++ Arrays/CollegeWallah/SecondLargest.java
@@ -4,13 +4,29 @@ import java.util.Arrays;
 
 public class SecondLargest {
     public static void main(String[] args) {
-        int[] arr ={1 ,1,6,7,12,32};
-    int ans =    findSecondLargest(arr);
+        int[] arr = {1, 5, 11, 23, 3, 8};
+        int ans = findSecondLargest(arr, arr.length);
         System.out.println(ans);
     }
-    static int findSecondLargest(int[] arr){
-        int max = Integer.MIN_VALUE;
-        return max;
+
+    static int findSecondLargest(int[] arr, int n) {
+        int max1 = findMax(arr, n);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == max1){
+                arr[i] = Integer.MIN_VALUE;
+            }
+        }
+       int max2 = findMax(arr,n);
+        return max2;
     }
 
+    static int findMax(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
